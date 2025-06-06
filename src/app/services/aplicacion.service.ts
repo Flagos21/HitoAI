225e57003fd6aaaa73c77560e39676ea1087a5f3
Flagos21 <114185228+Flagos21@users.noreply.github.com>
2025-06-06 00:54:51 -0400

    Add environment files and use apiUrl constant

--- src/app/services/aplicacion.service.ts
+++ src/app/services/aplicacion.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AplicacionService {
-  private apiUrl = 'http://localhost:3000/api/aplicacion';
+  private apiUrl = `${environment.apiUrl}/api/aplicacion`;
 
   constructor(private http: HttpClient) {}
 
