3ee89df0f25f2e118c022002b7a548119e987895
Flagos21 <114185228+Flagos21@users.noreply.github.com>
2025-06-10 21:55:01 -0400

    Revert "Add chart component"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChartComponent } from './chart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChartComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
