7f333e0c772ad6c197ef5d6f6bc5e17ec230966d
Flagos21 <114185228+Flagos21@users.noreply.github.com>
2025-06-15 20:11:33 -0400

    Revert "Gráficos individuales por indicador"

--- backend/services/informe.service.js
+++ backend/services/informe.service.js
@@ -356,15 +356,11 @@ exports.generarInforme = async asignaturaId => {
 
   const graficosInstancias = {};
   for (const [num, inst] of Object.entries(instancias)) {
-    graficosInstancias[num] = [];
-    for (const [idx, c] of inst.criterios.entries()) {
-      const path = await generarGraficoBarras(
-        [c.indicador],
-        [Number(c.porcentaje) || 0],
-        `instancia_${num}_${idx}.png`
-      );
-      graficosInstancias[num].push(path);
-    }
+    graficosInstancias[num] = await generarGraficoBarras(
+      inst.criterios.map(c => c.indicador),
+      inst.criterios.map(c => Number(c.porcentaje) || 0),
+      `instancia_${num}.png`
+    );
   }
 
   const resumenIndicadores = [...datos].sort((a, b) => a.instancia - b.instancia);
@@ -405,10 +401,7 @@ exports.generarInforme = async asignaturaId => {
   if (pdf.length) fs.writeFileSync(path.join(outDir, `${base}.pdf`), pdf);
 
   if (docx.length) fs.writeFileSync(path.join(outDir, `${base}.docx`), docx);
-  const archivosGraficos = [barrasPath, compPath];
-  Object.values(graficosInstancias).forEach(arr => {
-    archivosGraficos.push(...arr);
-  });
+  const archivosGraficos = [barrasPath, compPath, ...Object.values(graficosInstancias)];
   archivosGraficos.forEach(p => {
     if (fs.existsSync(p)) fs.unlinkSync(p);
   });
